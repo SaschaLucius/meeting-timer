484c389b7ed4936e9f1e85de7627e0e3c6260355
Sascha Lucius <sascha.lucius@posteo.de>
2024-12-02 23:34:51 +0100

    notifications

---
+++ notificationManager.js
@@ -0,0 +1,24 @@
+class NotificationManager {
+    constructor() {
+        this.notificationEnabled = false;
+    }
+
+    async requestNotificationPermission() {
+        if (!("Notification" in window)) {
+            console.log("This browser does not support notifications.");
+            return;
+        }
+        if (Notification.permission === "granted") {
+            this.notificationEnabled = true;
+        } else if (Notification.permission !== "denied") {
+            const permission = await Notification.requestPermission();
+            this.notificationEnabled = permission === "granted";
+        }
+    }
+
+    sendNotification(message) {
+        if (this.notificationEnabled && "Notification" in window) {
+            new Notification(message);
+        }
+    }
+}
