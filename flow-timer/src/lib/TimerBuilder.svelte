8609f00a6a8cb3c4cb666a6e4735120845584712
Sascha Lucius <sascha.lucius@posteo.de>
2024-12-29 15:00:17 +0100

    add timer builder

--- flow-timer/src/lib/TimerBuilder.svelte
+++ flow-timer/src/lib/TimerBuilder.svelte
@@ -1,167 +1,119 @@
 <script lang="ts">
-    interface Timer {
-        name: string,
-        duration?: string,
-        repetitions?: string,
-        timers?: Timer[],
-        description?: string
-    }
+	import TimerBlock from './TimerBlock.svelte';
 
-    // ####### Custom Timer Creator #######
-    let rootTimer : Timer = {
-        name: "",
-        duration: "00:00:00",
-        timers: [],
-        description: ""
-    };
+	// Converts total seconds to HH:mm:ss format
+	function secondsToHMS(totalSeconds: number | undefined): string {
+		if (totalSeconds === undefined) return '';
+		const hours = Math.floor(totalSeconds / 3600);
+		const minutes = Math.floor((totalSeconds % 3600) / 60);
+		const seconds = totalSeconds % 60;
 
-    function addSubTimer(timer:Timer = rootTimer) {
-        const subTimer = {
-            name: "",
-            duration: "",
-            repetitions: "",
-            timers: [],
-            description: ""
-        };
-        timer.duration = "";
-        if(timer.timers){
-            timer.timers.push(subTimer);
-        }else{
-            timer.timers = [subTimer];
-        }
-        renderTimers(rootTimer, document.getElementById("timerBuilder"));
-        updateTotalDuration();
-    }
+		// Format each unit to be two digits
+		return [hours, minutes, seconds].map((unit) => String(unit).padStart(2, '0')).join(':');
+	}
 
-    function deleteTimer(parent:Timer, timerIndex:number) {
-        parent.timers?.splice(timerIndex, 1);
-        renderTimers(rootTimer, document.getElementById("timerBuilder"));
-        updateTotalDuration();
-    }
+	interface Timer {
+		name: string;
+		duration?: number;
+		repetitions?: number;
+		timers?: Timer[];
+		description?: string;
+	}
 
-    function renderTimers(timer:Timer, container:any, parent : Timer | null = null, index: null | number = null) {
-        container.innerHTML = "";
-        const mainDiv = document.createElement("div");
-        mainDiv.className = "timer-block";
+	export let timer: Timer = { name: 'Timer', timers: [] };
 
-        // "×" delete button for each timer
-        if (parent !== null && index !== null) {
-            const deleteButton = document.createElement("span");
-            deleteButton.className = "delete-button";
-            deleteButton.innerText = "×";
-            deleteButton.onclick = () => deleteTimer(parent, index);
-            mainDiv.appendChild(deleteButton);
-        }
+	$: totalDuration = calculateTotalTime(timer);
 
-        // Timer name input
-        const nameInput = document.createElement("input");
-        nameInput.placeholder = "Name";
-        nameInput.value = timer.name || "";
-        nameInput.required = true;
-        nameInput.style.width = "25%";
-        nameInput.oninput = (e) => (timer.name = e.target.value);
+	function calculateTotalTime(timer: Timer) {
+		let totalTime = 0;
 
-        // Duration input
-        const durationInput = document.createElement("input");
-        durationInput.placeholder = "Duration HH:MM:SS";
-        durationInput.value = timer.duration || "";
-        durationInput.oninput = (e) => (timer.duration = e.target.value);
-        durationInput.disabled = timer.timers !== undefined && timer.timers.length > 0;
-        durationInput.style.width = "25%";
-        durationInput.onchange = (e) => updateTotalDuration();
+		// Parse the timer's duration and convert to seconds, defaulting to 0 if empty
+		const durationInSeconds = timer.duration ? timer.duration : 0;
 
-        // Repetitions input
-        const repetitionsInput = document.createElement("input");
-        repetitionsInput.placeholder = "Repetitions";
-        repetitionsInput.type = "number";
-        repetitionsInput.value = timer.repetitions || "";
-        repetitionsInput.min = "2";
-        repetitionsInput.style.width = "25%";
-        repetitionsInput.oninput = (e) => (timer.repetitions = e.target.value);
-        repetitionsInput.onchange = (e) => updateTotalDuration();
+		// Multiply by repetitions, defaulting to 1 if repetitions is not set or invalid
+		const repetitions = timer.repetitions && timer.repetitions > 1 ? timer.repetitions : 1;
+		totalTime += durationInSeconds * repetitions;
 
-        mainDiv.append(nameInput, durationInput, repetitionsInput);
+		// Recursively add time for each sub-timer
+		if (Array.isArray(timer.timers) && timer.timers.length > 0) {
+			timer.timers.forEach((subTimer) => {
+				totalTime += calculateTotalTime(subTimer) * repetitions;
+			});
+		}
 
-        // Description input
-        const descriptionInput = document.createElement("input");
-        descriptionInput.placeholder = "Description";
-        descriptionInput.value = timer.description || "";
-        descriptionInput.oninput = (e) => (timer.description = e.target.value);
-        descriptionInput.style.width = "calc(75% + 48px)";
+		return totalTime;
+	}
 
-        mainDiv.appendChild(descriptionInput);
+	function saveTimer() {
+		const cleanedRoot = cleanUpTimer(timer);
+		const savedTimers = JSON.parse(localStorage.getItem('savedTimers') + '') || {};
 
-        // List of sub-timers
-        const timerList = document.createElement("div");
-        timerList.className = "timer-list";
-        if (timer.timers) {
-            timer.timers.forEach((subTimer, subIndex) => {
-                const subTimerContainer = document.createElement("div");
-                subTimerContainer.className = "sub-timer";
-                renderTimers(subTimer, subTimerContainer, timer, subIndex);
-                timerList.appendChild(subTimerContainer);
-            });
-        }
-        mainDiv.appendChild(timerList);
+		if (savedTimers[cleanedRoot.name]) {
+			if (!confirm(`Timer '${cleanedRoot.name}' already exists. Do you want to overwrite it?`)) {
+				return; // Exit if the user declines to overwrite
+			}
+		}
 
-        // Add sub-timer button
-        const addSubTimerButton = document.createElement("button");
-        addSubTimerButton.innerText = "+";
-        addSubTimerButton.onclick = () => addSubTimer(timer);
-        mainDiv.appendChild(addSubTimerButton);
+		savedTimers[cleanedRoot.name] = cleanedRoot;
+		localStorage.setItem('savedTimers', JSON.stringify(savedTimers));
 
-        if (timer.timers && timer.timers.length > 0) {
-            // Collapse/Expand button for sub-timers
-            const toggleButton = document.createElement("button");
-            toggleButton.innerText = "Hide";
-            toggleButton.onclick = () => {
-                timerList.style.display = timerList.style.display === "none" ? "block" : "none";
-                toggleButton.innerText = timerList.style.display === "none" ? "Show" : "Hide";
-            };
-            mainDiv.appendChild(toggleButton);
-        }
+		// Select the newly saved timer in the selection
+		// const selectBox = document.getElementById('liberatingStructureSelect');
+		// TODO selectBox.value = cleanedRoot.name;
+	}
 
-        container.appendChild(mainDiv);
-    }
+	function cleanUpTimer(timer: Timer): Timer {
+		const cleanedTimer: Timer = { name: timer.name };
 
-    function cleanUpTimer(timer:Timer) : Timer {
-        const cleanedTimer : Timer = { name: timer.name };
+		if (timer.duration && timer.timers && timer.timers.length > 0) {
+			console.log(`${timer.name} cannot have both duration and sub-timers together`);
+			delete timer.duration;
+		}
+		if (timer.duration) cleanedTimer.duration = timer.duration;
+		if (timer.repetitions && Number(timer.repetitions) > 1)
+			cleanedTimer.repetitions = timer.repetitions;
+		if (timer.description) cleanedTimer.description = timer.description;
 
-        if (timer.duration && timer.timers && timer.timers.length > 0) {
-            console.log(`${timer.name} cannot have both duration and sub-timers together`);
-            delete timer.duration;
-        }
-        if (timer.duration) cleanedTimer.duration = timer.duration;
-        if (timer.repetitions && Number(timer.repetitions) > 1) cleanedTimer.repetitions = timer.repetitions;
-        if (timer.description) cleanedTimer.description = timer.description;
+		if (timer.timers && timer.timers.length > 0) {
+			cleanedTimer.timers = timer.timers.map(cleanUpTimer);
+		}
 
-        if (timer.timers && timer.timers.length > 0) {
-            cleanedTimer.timers = timer.timers.map(cleanUpTimer);
-        }
-
-        return cleanedTimer;
-    }
-
-    let totalDuration = "00:00:00";
-
-    function updateTotalDuration() {
-        const totalSeconds = calculateTotalTime(rootTimer);
-        totalDuration = secondsToHMS(totalSeconds);
-    }
-
+		return cleanedTimer;
+	}
 </script>
 
 <div>
-    <div id="timerBuilder"></div>
-    <div class="timer-info">Total Duration: <span id="totalDuration">{totalDuration}</span></div>
-    <!-- TODO button onclick={() => saveTimer()}>Save</button-->
-    <div id="errorMessage" style="color: red; margin-top: 5px;"></div>
+	<TimerBlock bind:timer />
+	<div class="timer-info">
+		Total Duration: <span id="totalDuration">{secondsToHMS(totalDuration)}</span>
+	</div>
+	<button onclick={() => saveTimer()}>Save</button>
+	<div id="errorMessage" style="color: red; margin-top: 5px;"></div>
 </div>
 
 <style>
-    .timer-block { position: relative; margin: 10px 0; padding: 10px; border: 1px solid #ccc; }
-    .timer-list { padding-left: 20px; }
-    .sub-timer { margin-top: 5px; padding-left: 15px; border-left: 1px dotted #ccc; }
-    .delete-button { position: absolute; top: 5px; right: 5px; cursor: pointer; color: red; font-weight: bold; }
-    input { margin: 5px; padding: 5px; }
+	button {
+		padding: 10px 20px;
+		margin: 5px;
+		border: none;
+		background-color: #007bff;
+		color: #fff;
+		border-radius: 5px;
+		cursor: pointer;
+		font-size: 16px;
+	}
+	button:hover {
+		background-color: #0056b3;
+	}
+	button:disabled {
+		background-color: #ccc;
+		cursor: not-allowed;
+	}
+	button {
+		margin: 5px;
+		padding: 5px 10px;
+	}
+	.timer-info {
+		font-size: 18px;
+	}
 </style>
