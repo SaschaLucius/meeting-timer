df0ffe5497127dd96555b1a379b91f7304d60803
Sascha Lucius <sascha.lucius@posteo.de>
2025-01-06 13:49:24 +0100

    add more info

--- flow-timer/src/lib/TimerBlock.svelte
+++ flow-timer/src/lib/TimerBlock.svelte
@@ -123,7 +123,7 @@
 	{#if editable}
 		<button onclick={addSubTimer}>+</button>
 	{/if}
-	<button onclick={() => (isHidden = !isHidden)}>{isHidden ? 'Show' : 'Hide'}</button>
+	<button onclick={() => (isHidden = !isHidden)}>{isHidden ? `Show (${timer.timers.length})` : 'Hide'}</button>
 </div>
 
 <style>
