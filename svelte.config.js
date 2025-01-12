92db293b8dfa63cd5ae9ab33cf3de92003814fad
Sascha Lucius <sascha.lucius@posteo.de>
2025-01-13 00:58:25 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -20,9 +20,9 @@ const config = {
 			precompress: false,
 			strict: true
 		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
+		//paths: {
+		//	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		//},
 		serviceWorker: {
 			register: false
 		}
