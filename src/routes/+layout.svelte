ebdc86b0a5d7b348cf632d90febe6e39e06f8b75
Sascha Lucius <sascha.lucius@posteo.de>
2025-01-17 00:07:53 +0100

    wave component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { pwaInfo } from 'virtual:pwa-info';
+	import Waves from '$lib/Waves.svelte';
 
 	onMount(async () => {
 		if (pwaInfo) {
@@ -29,56 +30,34 @@
 	{@html webManifest}
 </svelte:head>
 
-<main class="header">
-	<slot />
+<main>
+	<div class="header">
+		<slot />
 
-	<!--Waves Container Thank you to https://github.com/SayanBarcha/Simple-Waves -->
-	<div>
-		<svg
-			class="waves"
-			xmlns="http://www.w3.org/2000/svg"
-			xmlns:xlink="http://www.w3.org/1999/xlink"
-			viewBox="0 24 150 28"
-			preserveAspectRatio="none"
-			shape-rendering="auto"
-		>
-			<defs>
-				<path
-					id="gentle-wave"
-					d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
-				/>
-			</defs>
-			<g class="parallax">
-				<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
-				<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-				<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-				<use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
-			</g>
-		</svg>
-	</div>
-	<!--Waves end-->
+		<Waves />
 
-	<!--Content starts-->
-	<div class="content flex">
-		<p>Sascha Lucius</p>
-		<a
-			href="https://github.com/SaschaLucius/meeting-timer"
-			target="_blank"
-			aria-label="GitHub"
-			style="margin-left: 5px;"
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="16"
-				height="16"
-				viewBox="0 0 24 24"
-				fill="#333333"
+		<!--Content starts-->
+		<div class="content flex">
+			<p>Sascha Lucius</p>
+			<a
+				href="https://github.com/SaschaLucius/meeting-timer"
+				target="_blank"
+				aria-label="GitHub"
+				style="margin-left: 5px;"
 			>
-				<path
-					d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.385.6.11.82-.26.82-.577v-2.165c-3.338.726-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.756-1.333-1.756-1.09-.745.083-.73.083-.73 1.205.084 1.84 1.237 1.84 1.237 1.07 1.835 2.807 1.305 3.492.998.108-.775.42-1.305.763-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.467-2.38 1.235-3.22-.123-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.3 1.23.957-.266 1.98-.398 3-.403 1.02.005 2.043.137 3 .403 2.29-1.552 3.297-1.23 3.297-1.23.653 1.653.24 2.873.118 3.176.77.84 1.233 1.91 1.233 3.22 0 4.61-2.803 5.625-5.475 5.92.43.37.823 1.1.823 2.22v3.293c0 .32.218.694.825.577C20.565 21.8 24 17.3 24 12c0-6.63-5.37-12-12-12z"
-				/>
-			</svg>
-		</a>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					viewBox="0 0 24 24"
+					fill="#333333"
+				>
+					<path
+						d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.385.6.11.82-.26.82-.577v-2.165c-3.338.726-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.756-1.333-1.756-1.09-.745.083-.73.083-.73 1.205.084 1.84 1.237 1.84 1.237 1.07 1.835 2.807 1.305 3.492.998.108-.775.42-1.305.763-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.467-2.38 1.235-3.22-.123-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.3 1.23.957-.266 1.98-.398 3-.403 1.02.005 2.043.137 3 .403 2.29-1.552 3.297-1.23 3.297-1.23.653 1.653.24 2.873.118 3.176.77.84 1.233 1.91 1.233 3.22 0 4.61-2.803 5.625-5.475 5.92.43.37.823 1.1.823 2.22v3.293c0 .32.218.694.825.577C20.565 21.8 24 17.3 24 12c0-6.63-5.37-12-12-12z"
+					/>
+				</svg>
+			</a>
+		</div>
 	</div>
 </main>
 
